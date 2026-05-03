4d73b46dd31d328e3f8170f4db4d518afff3067e
nickslyadnev-coder <nick.slyadnev@gmail.com>
2026-05-03 16:11:28 +0300

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-ap
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '<h1>Привет, это моё приложение на Render!</h1><p>Работает на моём собственном сервере!</p>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
